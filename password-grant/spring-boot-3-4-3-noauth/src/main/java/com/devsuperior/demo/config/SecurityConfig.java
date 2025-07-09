a9d9dca4b1967e6be961169c26f74695f602b8b7
Bruno Dantas <brunoodantas7@gmail.com>
2025-07-09 15:35:50 -0300

    add: Spring Security dependency and basic HttpSecurity config to keep all routes avaiable without authentication.

---
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.csrf( csrfConfigurer -> csrfConfigurer.disable());
+        httpSecurity.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().permitAll());
+
+        return httpSecurity.build();
+    }
+}
